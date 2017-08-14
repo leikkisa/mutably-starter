e9c637e57ba9de3dfe9daa702e283bca7a2efbab
leikkisa <sallymaki@gmail.com>
2017-08-14 15:57:13 -0700

    getPokemon and new Pokemon form drafts

--- public/script.js
+++ public/script.js
@@ -2,6 +2,40 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
-  // code in here
+getPokemon()
+
+
 
 });
+
+function getPokemon() {
+  $.ajax({
+    method: 'GET',
+    url: 'http://mutably.herokuapp.com/pokemon'
+  }).done(function(data) {
+    data.pokemon.forEach((pokemon, i) => {
+      $('.list-group').append(`
+          <li class="list-group-item">
+            <img src="${pokemon.image}">
+            <p class="name">Name: ${pokemon.name}</p>
+            <p class="pokedex">Pokedex: ${pokemon.pokedex}</p>
+            <p class="evolves_from">Evolves from: ${pokemon.evolves_from}</p>
+            <button>Edit</button>
+            <button>Save</button>
+          </li>
+        `)
+    })
+  })
+}
+
+function addPokemon() {
+
+}
+
+function deletePokemon() {
+
+}
+
+function updatePokemon() {
+
+}
